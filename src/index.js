1a7719bbf1419a04a98d0fbeae260430c7b4e0d9
Elif <elifchorghay@gmail.com>
2022-12-26 23:19:59 +0300

    Add Tailwind Css with scss

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './styles/tailwind.scss';
 import App from './App';
+import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#ffffff',
+      'brand-primary': '#FF733C',
+      'brand-black': '#1B1B1B',
+    },
+  },
+  plugins: [],
+}
