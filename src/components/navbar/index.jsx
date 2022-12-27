ec65038c5544d28420dc69e1b91a6acd8d7e8a22
Elif <elifchorghay@gmail.com>
2022-12-28 00:54:54 +0300

    update navbar structure and style

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import logo from '../../assets/image/Logomark.png';
-import wrench from '../../assets/icon/wrench.png';
 import login from '../../assets/icon/login.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWrench } from '@fortawesome/free-solid-svg-icons';
 import './index.scss';
 
 export default function Navbar() {
   return (
     <div className='navbar'>
       <div className='navbar__right'>
-        <Link to="/" className='navbar__right-item'>
+        <Link className='navbar__right-item'>
           Apply
         </Link>
-        <Link to="/" className='navbar__right-item'>
+        <Link className='navbar__right-item'>
           How it works
         </Link>
-        <Link to="/" className='navbar__right-item'>
+        <Link className='navbar__right-item'>
           About us
         </Link>
-        <Link to="/" className='navbar__right-item'>
+        <Link className='navbar__right-item'>
           Blog
         </Link>
       </div>
@@ -26,11 +27,11 @@ export default function Navbar() {
         <img src={logo} alt="bumper-logo" />
       </div>
       <div className='navbar__left'>
-        <Link to="/" className='navbar__left-item'>
+        <Link className='navbar__left-item'>
           <span>Partner sign up</span>
-          <img src={wrench} alt="wrench-icon" />
+          <FontAwesomeIcon icon={faWrench} />
         </Link>
-        <Link to="/" className='navbar__left-item'>
+        <Link className='navbar__left-item'>
           <span>Partner login</span>
           <img src={login} alt="login-icon" />
         </Link>
