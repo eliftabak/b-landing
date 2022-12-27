04d9dcc84cf793d1d21de3e3fac28f889f950b62
Elif <elifchorghay@gmail.com>
2022-12-28 00:49:33 +0300

    restructure routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import RegisterFormPage from './pages/form'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register-form' element={<RegisterFormPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
