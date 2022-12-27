e96e6e1c69f1962fc9759a0da9e0d0f1b4d88c37
Elif <elifchorghay@gmail.com>
2022-12-28 00:50:47 +0300

    add colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ module.exports = {
   theme: {
     colors: {
       'white': '#ffffff',
-      'brand-primary': '#FF733C',
-      'brand-black': '#1B1B1B',
+      'brand': {
+        'primary': '#FF733C',
+        'secondary': '#5A698C',
+      },
+      'black': {
+        DEFAULT: '#000000',
+        '900': '#1B1B1B',
+      },
+      'gray': {
+        '900': '#545454',
+      },
+      'green': {
+        DEFAULT: '#32BE50',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'rajdhani': ['"Rajdhani"', 'sans-serif'],
+        'roboto': ['"Roboto"', 'sans-serif']
+      },
     },
   },
   plugins: [],
