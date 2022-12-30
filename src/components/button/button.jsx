e4611e9bf78c302e874051c2b67e0cb0d8592c06
Elif <elifchorghay@gmail.com>
2022-12-30 14:08:44 +0300

    Add button component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import './index.scss';
+import './button.scss';
 
 export default function Button() {
   return (
